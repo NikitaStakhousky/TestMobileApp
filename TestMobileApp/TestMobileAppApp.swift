9f21e99fbe6813834e1da4680ff1fceb34b6f79e
Nikita Stakhouski <nikitastakhousky@gmail.com>
2023-03-13 20:45:53 +0300

    Initial Commit

---
+++ TestMobileApp/TestMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMobileAppApp.swift
+//  TestMobileApp
+//
+//  Created by Nikita Stshovsky on 13.03.23.
+//
+
+import SwiftUI
+
+@main
+struct TestMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
